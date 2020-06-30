75598cf90a8a5f8ffe8bfe7fc6d721c5293ca910
David SG <dgouvea@macdep.clgxlabs.io>
2020-06-30 08:42:05 -0700

    Website Changes

---
+++ precache-manifest.8e230ee45c69f08eb05471fb3cbd9fb1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae1307139d9fe15366d35e8257fd8b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30e864cfcdf9c21b6586",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d0c52cfd65e63df90841",
+    "url": "/static/css/main.95106f67.chunk.css"
+  },
+  {
+    "revision": "30e864cfcdf9c21b6586",
+    "url": "/static/js/2.b99ee52d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b99ee52d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c52cfd65e63df90841",
+    "url": "/static/js/main.d1e476c2.chunk.js"
+  },
+  {
+    "revision": "2729d80ac497fc6b7466",
+    "url": "/static/js/runtime-main.9d8f3c50.js"
+  },
+  {
+    "revision": "94f41db36fd2c6908d948f81e66d6ed0",
+    "url": "/static/media/beach.94f41db3.jpg"
+  },
+  {
+    "revision": "d87cd1b222ba52a40bf79efc6ab7d0b9",
+    "url": "/static/media/bosque-do-alemao.d87cd1b2.jpg"
+  },
+  {
+    "revision": "b91b0e3b93a5dc3f8e9ab3d719259c9a",
+    "url": "/static/media/logo-md.b91b0e3b.png"
+  },
+  {
+    "revision": "43f685952139448bc1913457012245b6",
+    "url": "/static/media/praca-das-peraputangas.43f68595.jpg"
+  },
+  {
+    "revision": "911a69dde06255e1fe43ae37adde2162",
+    "url": "/static/media/waterfall.911a69dd.jpg"
+  }
+]);
