c660fb420d7d2110c2809f70ae1c59b5cfdd4838
David Sobreira Gouvea <davidsgbr@gmail.com>
2022-09-18 11:37:08 -0700

    Website Changes

---
+++ precache-manifest.9e684d0bcfd7beddc138594bb42d678a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d4297599a5d8c494808d63791ea63b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90cb9825714c2c271850",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "2c27c5acc456ac05d097",
+    "url": "/static/css/main.7bea4b37.chunk.css"
+  },
+  {
+    "revision": "90cb9825714c2c271850",
+    "url": "/static/js/2.8bbe2adc.chunk.js"
+  },
+  {
+    "revision": "b34a9f109935bcf1bb77d69170b15052",
+    "url": "/static/js/2.8bbe2adc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c27c5acc456ac05d097",
+    "url": "/static/js/main.42ce7644.chunk.js"
+  },
+  {
+    "revision": "38baf69fd4278342860a",
+    "url": "/static/js/runtime-main.58b5c65a.js"
+  },
+  {
+    "revision": "94f41db36fd2c6908d948f81e66d6ed0",
+    "url": "/static/media/beach.94f41db3.jpg"
+  },
+  {
+    "revision": "d87cd1b222ba52a40bf79efc6ab7d0b9",
+    "url": "/static/media/bosque-do-alemao.d87cd1b2.jpg"
+  },
+  {
+    "revision": "dcd84182e4135f0f68b7c943d04529d0",
+    "url": "/static/media/cida.dcd84182.jpg"
+  },
+  {
+    "revision": "b91b0e3b93a5dc3f8e9ab3d719259c9a",
+    "url": "/static/media/logo-md.b91b0e3b.png"
+  },
+  {
+    "revision": "43f685952139448bc1913457012245b6",
+    "url": "/static/media/praca-das-peraputangas.43f68595.jpg"
+  },
+  {
+    "revision": "911a69dde06255e1fe43ae37adde2162",
+    "url": "/static/media/waterfall.911a69dd.jpg"
+  }
+]);
